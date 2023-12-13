a889ff961a08610c34710fb70011fd30927fdf3d
mrottimista <adel.mohamed855@gmail.com>
2023-12-13 20:18:23 +0200

    fix: expose types

---
+++ index.ts
@@ -0,0 +1,23 @@
+import clear from "./src/helpers/clear";
+import get from "./src/helpers/get";
+import remove from "./src/helpers/remove";
+import set from "./src/helpers/set";
+
+const polyFillStorage = {
+  set: set,
+  get: get,
+  remove: remove,
+  clear: clear,
+};
+let AsyncStorage = {
+  local: polyFillStorage,
+  session: polyFillStorage,
+  sync: polyFillStorage,
+};
+
+// if running locally (outside the browser extension runtime)
+if (globalThis.chrome?.runtime?.id) {
+  AsyncStorage = chrome.storage;
+}
+
+export default AsyncStorage;
